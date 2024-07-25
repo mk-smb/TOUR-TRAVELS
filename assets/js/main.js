ccc0c608b8d407e1fd91e1ea1685ed6883104f5a
mk-smb <mayank241332@gmail.com>
2024-07-25 14:48:29 +0530

    all changes done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
 $(".testimonial-carousel").owlCarousel({
     items: 3,
-    margin: 10,
+    margin: 50,
     loop: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        768:{
+            items:2,
+        },
+        1200:{
+            items:3,
+        }
+    }
 })
 
 
@@ -13,3 +25,13 @@ $('.testimonial_next').click(function() {
 $('.testimonial_prev').click(function() {
     owl.trigger('prev.owl.carousel', [300]);
 })
+
+// navigation bar toggle
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector("#menu-toggle").addEventListener("click", () => {
+        const menu = document.querySelector("#menu");
+        menu.classList.toggle("hidden");
+        menu.classList.toggle("menu-open");
+    });
+});
+
